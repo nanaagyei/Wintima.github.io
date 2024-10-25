5d5857b9ea65e97ce0e205b5d84e6d1a522b83f8
nanaagyei <tuffourp@oregonstate.edu>
2024-10-24 18:25:19 -0700

    homepage update

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-let slideIndex = 0;
-showSlides();
+document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menuBtn');
+    const menu = document.querySelector('.menu');
+    const slideshowImages = document.querySelectorAll('.slideshow img');
+    let currentSlide = 0;
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    // Toogle Description Menu
+    const menuToggle = document.querySelector('.menu-toggle');
+    const description = document.getElementById('description');
+
+    menuToggle.addEventListener('mouseenter', function() {
+        description.classList.add('fade');
+    });
+
+    menuToggle.addEventListener('mouseleave', function() {
+        description.classList.remove('fade');
+    });
+
+    // Toggle menu
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('open');
+        menu.classList.toggle('open');
+        menuBtn.innerHTML = menuBtn.classList.contains('open') ?  '<i class="fa-solid fa-plus"></i>' : '<i class="fas fa-plus"></i>';
+    });
+
+    // Slideshow functionality
+    function showNextSlide() {
+        slideshowImages[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slideshowImages.length;
+        slideshowImages[currentSlide].classList.add('active');
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+
+    setInterval(showNextSlide, 4000);
+});
